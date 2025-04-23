2131560d0084e1c0d58fb50fc739d34cfcb03540
Jacob Stanton <bocaj527@gmail.com>
2025-04-23 17:35:41 -0400

    populates clipboard from text file

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,39 @@
 import java.awt.datatransfer.*;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.util.Scanner;
 import java.awt.Toolkit;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args) throws InterruptedException {
+        Scanner inputStream = null;
         Date today = new Date(LocalDateTime.now().getMonthValue(), LocalDateTime.now().getDayOfMonth());
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        String line;
 
-        //create first text and save it to the clipboard
-        StringSelection myText = new StringSelection("Seller info requested, DL " + today.getDeadline());
-        clipboard.setContents(myText, myText);
+        try {
+            inputStream = new Scanner(new FileInputStream("clipboard.txt"));
+        } catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
 
-        //create second text and save it to the clipboard
-        StringSelection myText1 = new StringSelection("some other text");
-        clipboard.setContents(myText1, myText1);
+        while (inputStream.hasNext()){
+            line = inputStream.nextLine();
+            if (line.contains("DL") || line.contains("UTL")){
+                line += " " + today.getDeadline();
+            }else if (line.contains("QQQ")){
+                int index = line.indexOf("QQQ");
+                line = line.substring(0,index) + today.getDeadline() + " " + line.substring(index+3);
+            }
+            StringSelection clip = new StringSelection(line);
+            clipboard.setContents(clip, null);
+            System.out.println("Adding to clipboard");
+            Thread.sleep(500);
+        }
+
+        StringSelection clip = new StringSelection(today.getDeadline());
+        clipboard.setContents(clip, null);
+        System.out.println("DONE");
     }
 }
